80fa39f0fb9ea7938aa10ccb68b5fd7b6e343dbc
OkamotoShu <okamotoshu11@gmail.com>
2026-09-07 14:37:36 +0900

    コミット確認

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="w-full border-b p-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">KSU EGG</h1>
+        {/* ナビゲーションメニューなどをここに追加 */}
+      </div>
+    </header>
+  );
+}
